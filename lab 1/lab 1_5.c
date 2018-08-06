c0f54bbeacdb253aba1f6579b8ba76fdfe7885ab
devanjali11 <42138974+devanjali11@users.noreply.github.com>
2018-08-06 15:43:42 +0530

    Add files via upload

---
+++ lab 1/lab 1_5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int i;
+char s[1000];
+cin>>"%s",s;
+for(i=0;s[i]!='\0';i++);
+
+cout<<("%d",i);
+}
---
+++ lab 2_3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespacestd;
+
+cout<<"enter the length of string";
+cin>>length;
+
+void minmax(int arr[length],int length,int *min,int *max)
+{
+	for(i=0;i<length;i++)
+	{
+		cout<<"enter the numbers";
+		cin>>arr[i];
+	}
+	for(i=0;i<length;i++)
+	{
+	if(arr[i]>arr[i+1]);
+{
+
+		arr[i]=max;
+}
+}
+cout<<max;	
+
+
+}
+}
